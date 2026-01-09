1a942cd8a24423dd6ab585ff9acb7e1e90b53327
Maksim05Cobtah <CobtahMitaap05@gmail.com>
2026-01-09 16:31:44 +0200

    @TEC-11 допрацював  додав анімацію за допомогою jawa-script

--- js/Model.js
+++ js/Model.js
@@ -6,3 +6,10 @@ function StartAn() {
     fot.classList.add("gradieentAnimation");
 }
 
+let an = document.getElementById("an2");
+
+window.addEventListener("scroll", anim2);
+
+function anim2() {
+    an.classList.add("divAn2");
+}
